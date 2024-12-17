a30542cf2ca72b55dbe8c4dfe9e2ce364c882227
AlexAirCG <alexairggg@yandex.ru>
2024-12-17 15:46:58 +0300

    feat: add base structure

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/shared/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button size="lg" variant="outline" className="hover:bg-slate-400 ">
+        Hello
+      </Button>
+    </div>
+  );
+}
