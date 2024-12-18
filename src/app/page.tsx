74e1bde874c555e6982b895e026a873f907f9819
AlexAirCG <alexairggg@yandex.ru>
2024-12-18 10:23:33 +0300

    feat: prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
   return (
     <div>
-      <Button size="lg" variant="outline" className="hover:bg-slate-400 ">
+      <Button
+        size="lg"
+        variant="destructive"
+        className="hover:bg-slate-400 mb-4"
+      >
         Hello
       </Button>
+      {games.map((game) => {
+        return (
+          <Card key={game.id} className="mb-4 p-3 w-52">
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
+      })}
     </div>
   );
 }
